c06d3bce39366d58e2a8704322c286a95af1fd3c
KahlilJoshua <136188721+KahlilJoshua@users.noreply.github.com>
2023-09-17 18:07:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slideIndex++;
-    
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
-    
-    setTimeout(showSlides, 2000);
+  let slides = document.getElementsByClassName("slide");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 2000);
 }
 
+const ul = document.getElementById("ul");
+document.getElementById("btn").onclick = function () {
+  if (ul.style.display == "flex") {
+    ul.style.display = "none";
+  } else {
+    ul.style.display = "flex";
+  }
+};
